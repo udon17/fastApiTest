a840feb45dd52fa839417aabbf676d3d34f10b07
udon17 <osakana012@gmail.com>
2024-07-16 20:37:03 +0900

    デバックができるようになった

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import logging
+
+# ログの設定
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
+    logger.debug("read_root関数が呼び出されました")
+    logger.debug("read_root関数が呼び出されました")
     return {"message": "Hello, World!"}
